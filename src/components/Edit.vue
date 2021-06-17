9542e011bb05393971c85064e284337fc9ba99b2
Your Name <ducnmth1907002@fpt.edu.vn>
2021-06-17 20:49:54 +0700

    first init

---
+++ src/components/Edit.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container p-4">
+    <div class="card">
+      <div class="card-header">
+        <h3>Update Employee</h3>
+      </div>
+      <div class="card-body">
+        <form v-on:submit.prevent="updateItem">
+          <div class="form-group">
+            <label>Name:</label>
+            <input type="text" class="form-control" v-model="item.name"/>
+          </div>
+          <div class="form-group">
+            <label>Wage:</label>
+            <input type="number" class="form-control" v-model="item.wage"/>
+          </div>
+          <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Update Employee"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: Number
+  },
+  data() {
+    return {
+      item: {}
+    }
+  },
+
+  created: function () {
+    this.getItem();
+  },
+
+  methods: {
+    getItem() {
+      let uri = 'http://localhost:8000/employees/' + this.id;
+      this.axios.get(uri).then((response) => {
+        this.item = response.data;
+      });
+    },
+
+    updateItem() {
+      let uri = 'http://localhost:8000/employees/' + this.id;
+      this.axios.put(uri, this.item).then(() => {
+        this.$emit('close')
+      });
+    }
+  }
+}
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>Items</h1>
+
+    <table class="table table-hover">
+      <thead>
+      <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Wage</td>
+        <td>Actions</td>
+      </tr>
+      </thead>
+
+      <tbody>
+      <tr v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.wage }}</td>
+        <td>
+          <button class="btn btn-primary" v-on:click="openModal(item.id)">Edit</button>
+          <button class="btn btn-danger ml-1" v-on:click="deleteItem(item.id)">Delete</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import Edit from "./Edit";
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+
+  created: function () {
+    this.fetchItems();
+  },
+
+  methods: {
+    fetchItems() {
+      let uri = 'http://localhost:8000/employees';
+      this.axios.get(uri).then((response) => {
+        this.items = response.data;
+      });
+    },
+    deleteItem(id) {
+      let uri = 'http://localhost:8000/employees/' + id;
+
+      this.axios.delete(uri).then(() => {
+        this.fetchItems()
+      });
+    },
+    openModal(id) {
+      const modalWidth = window && window.innerWidth > 800 ? '750px' : '95%'
+      this.$modal.show(Edit, {
+        id: id
+      }, {
+        width: modalWidth,
+        height: 'auto'
+      }, {
+        closed: this.fetchItems,
+      })
+    }
+  }
+}
+</script>
